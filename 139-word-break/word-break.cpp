e56660aa5dcd07a83cb2d558ed96a7595930efa2
vivekganta <vivekganta79@gmail.com>
2026-09-08 09:06:09 +0530

    Time: 13 ms (33.06%) | Memory: 17.5 MB (21.31%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    bool wordBreak(string s, vector<string>& wordDict) 
+    {
+        unordered_set<string>dict;
+        for (auto s : wordDict)
+        dict.insert(s);
+        int n = s.size();
+        vector<bool>dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                string temp = s.substr(j, i - j);
+                if (dict.count(temp) && dp[j])
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
