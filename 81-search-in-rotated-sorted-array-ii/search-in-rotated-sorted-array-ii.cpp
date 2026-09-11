6818cb41c2d6f31f564b64b00d40ab746ef0de3c
vivekganta <vivekganta79@gmail.com>
2026-09-11 15:23:21 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (8.27%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    bool search(vector<int>& nums, int target) 
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+            return true;
+
+            if (nums[left] == nums[mid] && nums[mid] == nums[right])
+            {
+                ++left;
+                --right;
+                continue;
+            }
+
+            if (nums[mid] >= nums[left])
+            {
+                if (nums[left] <= target && nums[mid] > target)
+                right = mid - 1;
+                else
+                left = mid + 1;
+            }
+            else
+            {
+                if (nums[mid] < target && nums[right] >= target)
+                left = mid + 1;
+                else
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+};
